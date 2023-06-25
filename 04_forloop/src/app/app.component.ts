2f76705be49132c5d0adebf8578a2a2466ec7868
Matthijs Kamstra <matthijs@fonk.amsterdam>
2023-06-25 20:03:30 +0200

    🐶

--- 04_forloop/src/app/app.component.ts
+++ 04_forloop/src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '04_forloop';
+	title = '04_forloop';
+
+	_stringArray: string[] = ['zero', 'one', 'two'];
+	_booleanArray: boolean[] = [true, false, true];
+	_numberArray: number[] = [0, 1, 2];
+
+	constructor() {
+		this.funcForLoop();
+		this.funcForIn();
+		this.funcForOf();
+	}
+
+	funcForLoop() {
+		console.log('function with for loop');
+		for (let i = 0; i < this._stringArray.length; i++) {
+			const _string = this._stringArray[i];
+			console.log(_string);
+		}
+	}
+
+	funcForIn() {
+		console.log('function with for in loop');
+		for (let i in this._stringArray) {
+			const _string = this._stringArray[i];
+			console.log(_string);
+		}
+	}
+
+	funcForOf() {
+		console.log('function with for of loop');
+		for (let x of this._stringArray) {
+			const _string = x;
+			console.log(_string);
+		}
+	}
 }
--- 05_functions/src/app/app.component.ts
+++ 05_functions/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '05_functions';
+	title = '05_functions';
+
+	hello(): string {
+		return 'Hello World';
+	}
+	helloName(name: string): string {
+		return `Hello ${name}`;
+	}
 }
