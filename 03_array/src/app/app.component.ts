0a4c98365b6ccfa3dfee0b03035ecf6397ed417f
Matthijs Kamstra <matthijs@fonk.amsterdam>
2023-06-25 17:59:28 +0200

    code for array

--- 03_array/src/app/app.component.ts
+++ 03_array/src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '03_array';
+	title = '03_array';
+
+	_stringArray: string[] = ['zero', 'one', 'two'];
+	_booleanArray: boolean[] = [true, false, true];
+	_numberArray: number[] = [0, 1, 2];
+
+	constructor() { }
+
+	func() {
+		for (let i = 0; i < this._stringArray.length; i++) {
+			const _string = this._stringArray[i];
+			console.log(_string);
+		}
+	}
 }
