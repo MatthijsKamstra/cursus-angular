b9cf37f74764e4a6275e33a6eef7ce0b4521a1f2
Matthijs Kamstra <matthijs@fonk.amsterdam>
2023-06-25 17:59:04 +0200

    for basic types

--- 02_basic_types/src/app/app.component.ts
+++ 02_basic_types/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '02_basic_types';
+	title = '02_basic_types';
+
+	_string: string = 'this is a string';
+	_boolean: boolean = true;
+	_number: number = 2;
 }
